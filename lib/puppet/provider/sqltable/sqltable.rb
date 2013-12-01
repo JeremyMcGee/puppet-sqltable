88d33dccc84826fbd0d851d52e24ab7e4101216a
Javier Palacios <javiplx@gmail.com>
2013-12-01 14:59:37 +0100

    Move instances method into provider

--- lib/puppet/provider/sqltable/sqltable.rb
+++ lib/puppet/provider/sqltable/sqltable.rb
@@ -1,20 +1,47 @@
 
 Puppet::Type.type(:sqltable).provide(:sqltable) do
 
+  def self.instances
+    resources = []
+
+    table = "Configuration"
+    columns = [ 'name' , 'value' , 'description' ]
+    propnames = [ :key , :value , :description ]
+
+    Dir.glob("/var/lib/mysql/*/%s.frm" % table).each do |frm|
+
+      database = frm.split('/')[-2]
+
+      command = ["mysql", "-A", "-B", "-N", "-D", :puppettest, '-e', "select %s from %s.%s" % [ columns.join(",") , database , table ] ]
+
+      Puppet::Util.execute(command).split("\n").each do |line|
+        items = line.split("\t")
+        resources << new( :name => "%s.%s.%s" % [ database , table , items[0] ] ,
+                          :ensure => :present ,
+                          :key => items[0] ,
+                          :value => items[1] ,
+                          :description => items[2] )
+      end
+
+    end
+
+    resources
+  end
+
   def exists?
-    !(resource[:ensure] == :absent or resource[:ensure].nil?)
+    !(@property_hash[:ensure] == :absent or @property_hash[:ensure].nil?)
   end
 
   def key
-    resource[:key]
+    @property_hash[:key]
   end
 
   def value
-    resource[:value]
+    @property_hash[:value]
   end
 
   def description
-    resource[:description]
+    @property_hash[:description]
   end
 
 end
--- lib/puppet/type/sqltable.rb
+++ lib/puppet/type/sqltable.rb
@@ -21,31 +21,5 @@ Puppet::Type.newtype(:sqltable) do
     desc "parameter description"
   end
 
-  def self.instances
-    resources = []
-
-    table = "Configuration"
-    columns = [ 'name' , 'value' , 'description' ]
-    propnames = [ :key , :value , :description ]
-
-    Dir.glob("/var/lib/mysql/*/%s.frm" % table).each do |frm|
-
-      database = frm.split('/')[-2]
-
-      command = ["mysql", "-A", "-B", "-N", "-D", :puppettest, '-e', "select %s from %s.%s" % [ columns.join(",") , database , table ] ]
-
-      Puppet::Util.execute(command).split("\n").each do |line|
-        items = line.split("\t")
-        resources << new( :name => "%s.%s.%s" % [ database , table , items[0] ] ,
-                          :key => items[0] ,
-                          :value => items[1] ,
-                          :description => items[2] )
-      end
-
-    end
-
-    resources
-  end
-
 end
 
