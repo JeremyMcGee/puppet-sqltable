4d3c274234a7ce6557028af08a4f01b07c8e1c8e
Javier Palacios <javiplx@gmail.com>
2013-12-01 10:55:38 +0100

    Add basic schema and sample content for table

---
+++ test/Configuration.sql
@@ -0,0 +1,7 @@
+
+CREATE TABLE Configuration (
+    name  varchar(20) not null,
+    value varchar(20) ,
+    description varchar(20)
+    );
+
---
+++ test/content.sql
@@ -0,0 +1,3 @@
+INSERT INTO Configuration VALUES
+  ('key','value','First description') ,
+  ('key2','value2','Description for second key') ;
