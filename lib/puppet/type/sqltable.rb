3c267e1dde259151d3a09de4f267dc85ca9e387f
Javier Palacios <javiplx@gmail.com>
2013-12-01 11:29:07 +0100

    Implement resource discovery

--- lib/puppet/type/sqltable.rb
+++ lib/puppet/type/sqltable.rb
@@ -15,5 +15,31 @@ Puppet::Type.newtype(:sqltable) do
     desc "parameter description"
   end
 
+  def self.instances
+    resources = []
+
+    table = "Configuration"
+    columns = [ 'name' , 'value' , 'description' ]
+    propnames = [ :keyname , :keyvalue , :keydescr ]
+
+    Dir.glob("/var/lib/mysql/*/%s.frm" % table).each do |frm|
+
+      database = frm.split('/')[-2]
+
+      command = ["mysql", "-A", "-B", "-N", "-D", :puppettest, '-e', "select %s from %s.%s" % [ columns.join(",") , database , table ] ]
+
+      Puppet::Util.execute(command).split("\n").each do |line|
+        items = line.split("\t")
+        name = "%s.%s.%s" % [ database , table , items[0] ]
+        obj = Puppet::Resource.new('sltable', name )
+        propnames.zip( items ).each{ |k,v| obj[k] = v }
+        resources << obj
+      end
+
+    end
+
+    resources
+  end
+
 end
 
