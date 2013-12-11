2243ba7523d1e861ad1168d578a4a1d9c3cd41e1
Javier Palacios <javiplx@gmail.com>
2013-12-11 23:09:11 +0100

    Detect conflicts on manifest

--- lib/puppet/type/sqltable.rb
+++ lib/puppet/type/sqltable.rb
@@ -18,6 +18,11 @@ Puppet::Type.newtype(:sqltable) do
 
   newproperty(:key) do
     desc "parameter name"
+    validate do |value|
+      if not resource[:key].nil? and value != resource[:key]
+        fail( "Conflicting values for key name : %s vs %s" % [ value , resource[:key] ] )
+      end
+    end
   end
 
   newproperty(:value) do
@@ -30,6 +35,11 @@ Puppet::Type.newtype(:sqltable) do
 
   newparam(:database) do
     desc "database name"
+    validate do |value|
+      if not resource[:database].nil? and value != resource[:database]
+        fail( "Conflicting values for database : %s vs %s" % [ value , resource[:database] ] )
+      end
+    end
   end
 
 end
